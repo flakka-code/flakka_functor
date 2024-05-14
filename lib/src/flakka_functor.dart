8f894a21ef4682bbe3cf6eebfed630e8eb9c38b8
ryanhanks-bestow <ryan.hanks@bestow.world>
2024-05-14 12:31:15 -0500

    wipwip

--- lib/src/flakka_functor.dart
+++ lib/src/flakka_functor.dart
@@ -1,3 +1,5 @@
+import 'package:meta/meta.dart';
+
 /// {@template flakka_functor}
 /// A model for dispatching and handling typed message-replies
 /// {@endtemplate}
@@ -5,3 +7,61 @@ class FlakkaFunctor {
   /// {@macro flakka_functor}
   const FlakkaFunctor();
 }
+
+///
+// ignore: one_member_abstracts
+abstract base class RefProvider<Message, Reply, Props> {
+  ///
+  Ref<Message, Reply> get(Props props);
+}
+
+///
+abstract class Ref<Message, Reply> {
+  ///
+  Reply send(Message message);
+
+  ///
+  Ref<MessageT, ReplyT> ref<MessageT, ReplyT, PropsT>(PropsT props);
+}
+
+///
+abstract base class Behavior<Message, Reply, Props> {
+  ///
+  late String _props;
+
+  ///
+  @protected
+  set props(String props) => _props = props;
+
+  ///
+  @protected
+  String get props => _props;
+
+  ///
+  Reply receive(Message message);
+
+// ///
+// FunctorRef<MessageT, ReplyT> ref<MessageT, ReplyT, PropsT>(
+//     FunctorRefProvider<MessageT, ReplyT, PropsT> provider, PropsT props);
+}
+
+/// A behavior that operates on a state value (A StatefulFunctor?,
+abstract base class StateBehavior<Message, State, Reply, Props>
+    extends Behavior<Message, Reply, Props> {
+  ///
+  @protected
+  State get state;
+
+  @protected
+  set state(State state);
+}
+
+///
+// final class ActionBehavior<Action,State,Error,>{}
+class MutationBehavior {}
+
+///
+class ProjectionBehavior {}
+
+///
+class ActionBehavior {}
--- test/src/flakka_functor_test.dart
+++ test/src/flakka_functor_test.dart
@@ -1,11 +1,62 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flakka_functor/flakka_functor.dart';
+import 'package:meta/meta.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:test/test.dart';
 
+import 'util.dart';
+
+part 'flakka_functor_test.g.dart';
+
 void main() {
+  test('Some description', () {
+    // Create a ProviderContainer for this test.
+    // DO NOT share ProviderContainers between tests.
+    final container = createContainer();
+    final ref = container.read(myNotifierProvider);
+    // ref.
+    expect(
+      ref.send('Hello world'),
+      equals(12),
+    );
+  });
   group('FlakkaFunctor', () {
     test('can be instantiated', () {
       expect(FlakkaFunctor(), isNotNull);
     });
   });
+}
+
+@riverpod
+class MyNotifier extends _$MyNotifier {
+  @override
+  Functor1Ref build() {
+    throw UnimplementedError();
+  }
+}
+
+final class Functor1Behavior extends Behavior<String, String, String> {
+  @override
+  String receive(String message) {
+    return '$message $props';
+  }
+}
+
+class Functor1Ref extends Ref<String, int> {
+  @override
+  Ref<MessageT, ReplyT> ref<MessageT, ReplyT, PropsT>(PropsT props) {
+    throw UnimplementedError();
+  }
+
+  @override
+  int send(String message) {
+    throw UnimplementedError();
+  }
+}
+
+///
+@riverpod
+String myFunction(MyFunctionRef ref) {
+  // <your logic here>
+  return 'Hello world';
 }
